31b8534f6b63f496aec24057bcfc33694e4694bd
Dilven <mateusiakkrystian@gmail.com>
2021-10-06 00:08:10 +0200

    Change auth flow

---
+++ hooks/useAuth.ts
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import AccountsSDK from '@livechat/accounts-sdk';
+
+const options = {
+    client_id: 'be4050fbda6ee6a63636bfa0ea4554cd'
+};
+
+const useAuth = () => {
+    const [authData, setAuthData] = useState(null);
+    const [isLogged, setIsLogged] = useState(false);
+    const [isLogging, setIsLogging] = useState(false);
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const instance = new AccountsSDK(options);
+                const authData = await instance.iframe(options).authorize();
+                setIsLogged(true);
+                setAuthData(authData);
+            } catch (error) {
+                console.log('error', error);
+            } finally {
+                setIsLogging(false);
+            }
+        })();
+    }, []);
+    console.log(authData);
+
+    return {
+        authData,
+        isLogged,
+        isLogging,
+    };
+};
+
+export default useAuth;
