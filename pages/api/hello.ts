e47e0c03295d474e171b9b46d1f393df60b731cb
Dilven <mateusiakkrystian@gmail.com>
2021-10-06 00:30:29 +0200

    Add basic api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import Boom from '@hapi/boom';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { methodHandler } from '../../helpers/method-handler';
 
-type Data = {
-  name: string
-}
+const getHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    res.status(200).json({ message: 'hello' });
+  } catch (e) {
+    // logger.info(`Bad search request: ${query}`);
+    console.log(e)
+    throw Boom.badRequest((e as Error).message, e);
+  }
+};
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
-}
+export default methodHandler({ get: getHandler });
